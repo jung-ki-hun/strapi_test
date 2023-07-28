c46f277a88b5c5cb14c8e777c56fb2e525aab21e
jungkihun <pocket.jkh@gmail.com>
2023-07-28 16:35:42 +0900

    jkh strapi 시작

---
+++ test/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5725d5e7058756bded3051283859942c'),
+    },
+  },
+});
